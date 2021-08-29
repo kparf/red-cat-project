06b04737cbb964d8a85780da89d2bed43ec2d434
Kiryl Parfiankou <kiryl.parfiankou.dev@gmail.com>
2021-08-29 18:09:17 +0300

    Add navbar

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require(`tailwindcss`)(`./tailwind.config.js`),
+    require(`postcss-import`),
     require(`autoprefixer`),
     ...(process.env.NODE_ENV === "production"
       ? [
